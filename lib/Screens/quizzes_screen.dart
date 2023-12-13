82c0c1d972811c5bc2718c128476e078c9128581
fatimaBenazzou <b.fati2102@gmail.com>
2023-12-13 18:41:13 +0100

    quizzes ui

--- lib/Screens/quizzes_screen.dart
+++ lib/Screens/quizzes_screen.dart
@@ -17,89 +17,164 @@ class _QuizzesScreenState extends State<QuizzesScreen> {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(40),
-      child: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: ExpansionPanelList.radio(
-            elevation: 1,
-            expandIconColor: Colors.white,
-            expandedHeaderPadding: const EdgeInsets.all(0),
-            children: questions.map((QuizQuestion qst) {
-              return ExpansionPanelRadio(
-                backgroundColor: ThemeConfig.primaryColor,
-                value: qst.id,
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return Column(
-                    children: [
-                      ListTile(
-                        leading: Image.asset('assets/images/quizz.png'),
-                        title: Text(
-                          'Quizz ${qst.id}',
-                          style: ThemeConfig.caption.copyWith(
-                            color: ThemeConfig.buttonColor,
+      child: ListView(
+        children: questions
+            .map((QuizQuestion qst) => Card(
+              
+                  color: ThemeConfig.primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  elevation: 3,
+                  // margin: const EdgeInsets.only(bottom: 20),
+                  child: ExpansionTile(
+                    iconColor: ThemeConfig.buttonColor,
+                    tilePadding: const EdgeInsets.all(0),
+                    title: Column(
+                      children: [
+                        ListTile(
+                          leading: Image.asset('assets/images/quizz.png'),
+                          title: Text(
+                            'Quizz ${qst.id}',
+                            style: ThemeConfig.caption.copyWith(
+                              color: ThemeConfig.buttonColor,
+                            ),
                           ),
                         ),
-                      ),
-                      ListTile(
-                        title: Text(
-                          'You are doing great in terms of levels solve more quizzes to earn rewards, keep it up !',
-                          style:
-                              Theme.of(context).textTheme.bodySmall!.copyWith(
-                                    color: Colors.grey,
-                                    fontSize: 10,
-                                  ),
+                        ListTile(
+                          title: Text(
+                            'You are doing great in terms of levels solve more quizzes to earn rewards, keep it up !',
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      color: Colors.grey,
+                                      fontSize: 10,
+                                    ),
+                          ),
                         ),
-                      ),
-                    ],
-                  );
-                },
-                body: Container(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(30),
-                        bottomRight: Radius.circular(30)),
-                    color: ThemeConfig.backgroundColor,
-                  ),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                      ],
+                    ),
                     children: [
-                      Text(
-                        qst.title,
-                      ),
-                      Text(
-                        qst.question,
-                      ),
-                      const SizedBox(height: 30),
-                      ...qst.shuffledAnswers.map((answer) {
-                        return SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            style: TextButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(bottomRight: Radius.circular(20), bottomLeft: Radius.circular(20)),
+                          color: ThemeConfig.backgroundColor
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                qst.title,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
                               ),
-                            ),
-                            child: Text(
-                              answer,
-                              style: ThemeConfig.squirkButton
-                                  .copyWith(fontSize: 12),
-                            ),
-                            onPressed: () {},
+                              const SizedBox(height: 10),
+                              Text(
+                                qst.question,
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                              const SizedBox(height: 20),
+                              ...qst.answers.map((answer) {
+                                return SizedBox(
+                                  width: double.infinity,
+                                  child: ElevatedButton(
+                                    style: TextButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(24),
+                                      ),
+                                    ),
+                                    child: Text(
+                                      answer,
+                                      style: ThemeConfig.squirkButton
+                                          .copyWith(fontSize: 12),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                );
+                              }),
+                            ],
                           ),
-                        );
-                      }),
+                        ),
+                      ),
                     ],
                   ),
-                ),
-              );
-            }).toList(),
-          ),
-        ),
+                ))
+            .toList(),
       ),
     );
   }
 }
+//  ...questions.map((QuizQuestion qst) =>
+//                  ExpansionPanelRadio(
+//                   backgroundColor: ThemeConfig.primaryColor,
+//                   value: qst.id,
+//                   headerBuilder: (BuildContext context, bool isExpanded) {
+//                     return Column(
+//                       children: [
+//                         ListTile(
+//                                                     leading: Image.asset('assets/images/quizz.png'),
+//                           title: Text(
+//                             'Quizz ${qst.id}',
+//                             style: ThemeConfig.caption.copyWith(
+//                               color: ThemeConfig.buttonColor,
+//                             ),
+//                           ),
+//                         ),
+//                         ListTile(
+//                           title: Text(
+//                             'You are doing great in terms of levels solve more quizzes to earn rewards, keep it up !',
+//                             style:
+//                                 Theme.of(context).textTheme.bodySmall!.copyWith(
+//                                       color: Colors.grey,
+//                                       fontSize: 10,
+//                                     ),
+//                           ),
+//                         ),
+//                       ],
+//                     );
+//                   },
+//                   body: Container(
+//                     decoration: const BoxDecoration(
+//                       borderRadius: BorderRadius.only(
+//                           bottomLeft: Radius.circular(30),
+//                           bottomRight: Radius.circular(30)),
+//                       color: ThemeConfig.backgroundColor,
+//                     ),
+//                     padding:
+//                         const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Text(
+//                           qst.title,
+//                         ),
+//                         Text(
+//                           qst.question,
+//                         ),
+//                         const SizedBox(height: 30),
+//                         ...qst.shuffledAnswers.map((answer) {
+//                           return SizedBox(
+//                             width: double.infinity,
+//                             child: ElevatedButton(
+//                               style: TextButton.styleFrom(
+//                                 shape: RoundedRectangleBorder(
+//                                   borderRadius: BorderRadius.circular(24),
+//                                 ),
+//                               ),
+//                               child: Text(
+//                                 answer,
+//                                 style: ThemeConfig.squirkButton
+//                                     .copyWith(fontSize: 12),
+//                               ),
+//                               onPressed: () {},
+//                             ),
+//                           );
+//                         }),
+//                       ],
+//                     ),
+//                   ),
+//                 );
+//               ).toList(),
