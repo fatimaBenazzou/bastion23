88a16949066565ec0b1afd3dfc01ac5fceeb6c03
fatimaBenazzou <b.fati2102@gmail.com>
2023-12-14 02:57:06 +0100

    tabs

---
+++ lib/Screens/2d_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class DScreen extends StatelessWidget {
+  DScreen({super.key});
+  final List<String> images = List.generate(9, (index) => 'assets/images/image$index.jpg');
+
+  @override
+  Widget build(BuildContext context) {
+    return  GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+        ),
+        itemCount: images.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              _showImagePopup(context, index);
+            },
+            child: Image.asset(
+              images[index],
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+      );
+  }
+
+  
+  void _showImagePopup(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        void goToNextImage() {
+          if (index < images.length - 1) {
+            Navigator.of(context).pop();
+            _showImagePopup(context, index + 1);
+          }
+        }
+
+        void goToPreviousImage() {
+          if (index > 0) {
+            Navigator.of(context).pop();
+            _showImagePopup(context, index - 1);
+          }
+        }
+
+        return Dialog(
+        child: SizedBox(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Expanded(
+               child: Image.asset(
+                    images[index],
+                    fit: BoxFit.contain,
+                  ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton.outlined(
+                    icon: const Icon(Icons.arrow_back),
+                    color: Colors.white,
+                    onPressed: goToPreviousImage,
+                  ),
+                  IconButton.outlined(
+                    icon: const Icon(Icons.arrow_forward),
+                    color: Colors.white,
+                    onPressed: goToNextImage,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+      }
+    );
+  }
+}
+
---
+++ lib/Screens/3d_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ModelScreen extends StatefulWidget {
+  const ModelScreen({super.key});
+
+  @override
+  State<ModelScreen> createState() => _ModelScreenState();
+}
+
+class _ModelScreenState extends State<ModelScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
